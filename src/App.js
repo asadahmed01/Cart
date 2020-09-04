0cec94be088e7d415f544091ec1b9a2b517443af
lantanakamara <asadatnish@gmail.com>
2020-09-04 09:42:50 -0400

    creating the file structure for simple react shopping cart to demonstrate how react state is lifted up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Order from './components/Order';
+
+function App() {
+  return (
+    <div>
+      <Order />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Order = () => {
+
+
+  return (
+    <h1>Main Shopping Cart</h1>
+  )
+}
+
+export default Order;
